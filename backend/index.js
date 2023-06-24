c0c8cd80f09fbad137772a2363fb8731ba95b600
Nur Mohammad <mohammadnur5480@gmail.com>
2023-06-24 10:00:52 +0600

    Get all orders

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000;
 const cors = require('cors')
+const orders = require('./data/orders.json')
+
+app.use(cors())
+app.get('/orders' , (req, res)=>{
+    res.send(orders)
+})
 
 
 
